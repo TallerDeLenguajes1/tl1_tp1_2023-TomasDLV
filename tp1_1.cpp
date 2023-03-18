a0da33f9dcea0c4b32538fa0236207a24f89427b
TomasDLV <tomas01dlv@gmail.com>
2023-03-18 00:31:10 -0300

    Punto 3 f realizado

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,4 +1,13 @@
 #include <stdio.h>
-void main(){
+int main(){
     printf("Hola mundo");
+    int *punt,var;
+    punt = &var;
+
+    printf("\nEl contenido del puntero es: %d",*punt);
+    printf("\nLa direccion de memoria almacenada por el puntero es: %d",punt);
+    printf("\nLa direccion de memoria de la variable es: %d",&var);
+    printf("\nLa direccion de memoria del puntero es: %d",&punt);
+    printf("\nEl tamanio de memoria utilizado por la variable es: %d bytes",sizeof(var));
+    return 0;
 }
